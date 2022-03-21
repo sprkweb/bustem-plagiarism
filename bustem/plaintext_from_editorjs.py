41c2d0aecfd758b42694c175c55dba19332a7887
Vadim Saprykin <sprkweb@yandex.ru>
2022-03-21 17:37:53 +0300

    support more block types for EditorJS

--- bustem/plaintext_from_editorjs.py
+++ bustem/plaintext_from_editorjs.py
@@ -4,6 +4,13 @@ def plaintext_from_editorjs(editorjs_data: dict) -> str:
     for block in editorjs_data['blocks']:
         if 'text' in block['data']:
             text.append(block['data']['text'])
+        elif block['type'] == 'list':
+            for item in block['data']['items']:
+                text.append(item)
+        elif block['type'] == 'image':
+            pass
+        elif block['type'] == 'code':
+            pass
         else:
             print('Unknown block type: ' + block['type'])
     return ' '.join(text)
