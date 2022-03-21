f9559eaaa90341c4745ad529f1151de8810965ef
Vadim Saprykin <sprkweb@yandex.ru>
2022-03-21 19:01:48 +0300

    bugfix: comparision does not work with empty texts

--- bustem/compare_ngrams.py
+++ bustem/compare_ngrams.py
@@ -1,4 +1,8 @@
 # Returns a score from 0 to 1: How similar are the documents?
 # Score is the size of intersection of sets divided by the size of their union.
 def compare_ngrams(ngrams1: set, ngrams2: set) -> int:
-    return len(ngrams1.intersection(ngrams2)) / len(ngrams1.union(ngrams2))
+    intersection = len(ngrams1.intersection(ngrams2))
+    union = len(ngrams1.union(ngrams2))
+    if union == 0:
+        return 0
+    return intersection / union
